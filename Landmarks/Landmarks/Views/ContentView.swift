3e9a2e5dc4631530b3580b483d5ecab0b025fa4f
augday <jianfeng.zhang0411@foxmail.com>
2022-08-24 22:28:56 +0800

    add apple swiftUI demo

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by augday on 2022/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(ModelData())
+    }
+}
